513087d69a4ac19cb709190b97ce6530cc044803
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-05-07 00:27:00 +0200

    Time: 21 ms (81.01%), Space: 51.1 MB (88.87%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    Queue <Integer> queue;
+    public RecentCounter() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public int ping(int t) {
+        while (!queue.isEmpty() && (t - queue.peek()) > 3000){
+            queue.poll();
+        }
+        queue.offer(t);
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
