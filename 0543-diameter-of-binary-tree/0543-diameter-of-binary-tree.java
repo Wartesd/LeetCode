0c508a287f522c6f2da1b028028e1b44144f4429
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-05-21 00:48:01 +0200

    Time: 0 ms (100%), Space: 47.1 MB (45.81%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    public int dfs(TreeNode root){
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        max = Math.max(max, left+right);
+        return 1+Math.max(left,right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
