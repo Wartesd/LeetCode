77fa6b03f5133894fd49d5ba4f9e33df0954735c
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-06-09 21:49:20 +0200

    Time: 0 ms (100%), Space: 42.1 MB (34.43%) - LeetHub - Time: O(n), Space: O(1)

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int [n];
-        for (int i=0; i<n; i++){
-            if (i < 2){
-                dp[i] =i+1;
-            }else{
-               dp[i] = dp[i-1] + dp[i-2]; 
+        int prev1 = 1;
+        int prev2 = 2;
+        int result = 0;
+        for (int i=1; i<=n; i++){
+            if (i<3) result = i;
+            else{
+                result = prev1+prev2;
+                prev1 = prev2;
+                prev2 = result;
             }
         }
-        return dp[n-1];
+        return result;
     }
 }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
