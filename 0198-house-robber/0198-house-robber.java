479dc848c965b44c9c2572a0ad40b2e3007db570
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-06-10 22:32:18 +0200

    Time: 0 ms (100%), Space: 42.9 MB (36.33%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1 = 0;
+        int prev2 = 0;
+        int result = 0;
+        for (int i=0; i<nums.length; i++){
+            if (i == 0){
+                prev1 = nums[0];
+                result = prev1;
+            }
+            else if (i==1){
+                result = Math.max(prev1,nums[1]);
+                prev2 = result;
+            } 
+            else{
+                result = Math.max(prev1+nums[i],prev2);
+                prev1=prev2;
+                prev2=result;
+            }
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
