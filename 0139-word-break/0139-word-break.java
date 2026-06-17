9c0cd2656628a277e99f2e53f0277cf2b7a1cd7d
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-06-17 22:35:11 +0200

    Time: 9 ms (25.55%), Space: 46.4 MB (11.68%) - LeetHub - Time: O(n^2 * m), Space: O(n)

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] =true;
+        for (int i=1; i<= s.length(); i++){
+            for (int j=0; j<i;j++){
+                if (dp[j] == true && wordDict.contains(s.substring(j,i))){
+                    dp[i] =true;
+                    break;
+                } 
+            }
+        }
+        return dp[s.length()];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
