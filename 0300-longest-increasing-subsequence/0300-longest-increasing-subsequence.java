b0e1564de0ab67cff47bad8fb777050844f8f10f
Wartesd <167800674+Wartesd@users.noreply.github.com>
2026-06-19 23:36:21 +0200

    Time: 53 ms (28.69%), Space: 46.1 MB (68.83%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        for (int i=1; i<dp.length; i++){
+            for (int j=0; j<i; j++){
+                if (nums[i] <= nums[j]) continue;
+                dp[i] = Math.max(dp[j]+1, dp[i]);
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
