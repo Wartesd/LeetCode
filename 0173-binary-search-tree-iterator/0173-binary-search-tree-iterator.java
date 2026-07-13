120b6935c2ab94c8bd9fb9c87baccc69f1205a10
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-07-13 21:53:27 +0200

    Time: 17 ms (95.43%), Space: 49.8 MB (97.5%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    private Deque <TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new ArrayDeque<>();
+        while (root != null){
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode curr = stack.pop();
+        int result = curr.val;
+        curr = curr.right;
+        while (curr!=null){
+            stack.push(curr);
+            curr = curr.left;
+        }
+        return result;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();       
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
