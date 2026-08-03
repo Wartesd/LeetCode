1cdc778fe75dd95265ed404d0c39cf86bb9deeb6
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-08-03 22:36:30 +0200

    Time: 0 ms (100%), Space: 43.3 MB (78.45%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while (mid<=high){
+            int current = nums[mid];
+            if (current == 0){
+                nums[mid] = nums[low];
+                nums[low] = current;
+                low++;
+                mid++;
+            }else if(current == 1){
+                mid++;
+            }else{
+                nums[mid] = nums[high];
+                nums[high] = current;
+                high--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
