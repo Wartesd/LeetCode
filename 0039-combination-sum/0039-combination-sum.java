e93fe43d5cb4b85dcef7839999c2f4a687f61a75
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-08-30 18:28:52 +0200

    Time: 2 ms (95.55%), Space: 45.6 MB (83.06%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+
+        backtracking(candidates, target,0,combinations, new ArrayList<>());
+        return combinations; 
+    }
+    private void backtracking (int[] candidates,int target,int start,List<List<Integer>> combinations,List<Integer> current ){
+        if (target < 0) return;
+        if (target == 0){
+            combinations.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i =start; i<candidates.length; i++){
+            current.add(candidates[i]);
+            backtracking(candidates, target-candidates[i],i,combinations, current);
+            current.remove(current.size()-1);
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
