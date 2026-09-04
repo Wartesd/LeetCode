6f1d49cacb0a03b5f17936245bb7033e6f35c964
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-09-04 20:34:13 +0200

    Time: 19 ms (19.88%), Space: 45.7 MB (31.51%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == target) return sum;
+
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) closestSum = sum;
+                if (sum > target) right--;
+                else left++;
+            }
+        }
+        return closestSum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
