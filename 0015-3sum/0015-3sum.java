958bae91551790043a01ff590eb770f0447d3b82
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-09-05 20:47:05 +0200

    Time: 31 ms (81.83%), Space: 59.3 MB (32.08%) - LeetHub - Time: O(n^2), Space: O(log n) or O(n)

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -22,7 +22,7 @@ class Solution {
 
 
                     while (left < right && nums[left] == nums[left-1]) left++;
-                    while (left < right && nums[right] == nums[left-1]) right--;
+                    while (left < right && nums[right] == nums[right+1]) right--;
                 }
             }
         }
