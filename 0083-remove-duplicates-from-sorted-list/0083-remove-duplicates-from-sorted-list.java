74509310fccd59228683e38ed1a1338800a5c9bf
Anton Skidan <167800674+Wartesd@users.noreply.github.com>
2026-09-09 20:13:44 +0200

    Time: 0 ms (100%), Space: 45.2 MB (92.97%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode curr = head;
+
+        while (curr.next != null){
+            if (curr.next.val == curr.val){
+                curr.next = curr.next.next;
+                continue; 
+            }
+
+            curr = curr.next;
+        }
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
